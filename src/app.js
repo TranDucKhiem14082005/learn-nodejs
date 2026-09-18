5703c79b7e91ab101c16d64a64c736d355063e07
TranDucKhiem14082005 <khiemtran015@gmail.com>
2026-09-18 21:34:43 +0700

    learn-nodejs

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(bodyParser.json());
 
 app.get("/khiem", (req, res) => {
     console.log("Client doing call a api");
-    res.send('Hello World!');
+    res.send('Hello World! and today i promise with myself that i will do my best to learn and practice more and more');
 });
 
 module.exports = app;
